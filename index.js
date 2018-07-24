0cde7395785fb73cfed4d0177c7964c1bf2ac6da
Matt Koster <mattkoster@gmail.com>
2018-07-24 17:15:22 -0500

    Added CORS headers.

--- index.js
+++ index.js
@@ -26,6 +26,12 @@ var request_options = {
 
 const app = express()
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/', function (req, res) {
   getVehicles( function(my_results) {
     res.send(JSON.stringify(my_results))
