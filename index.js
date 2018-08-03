fdad185bd98641276cfecd05ba9eb76c9499cbe5
Matt Koster <mattkoster@gmail.com>
2018-08-03 01:14:41 -0500

    Added additional routes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const express = require('express')
 const BUSTIME_URL = 'http://realtime.ridemcts.com/bustime/api/v1/getvehicles'
 const BUSTIME_API_KEY = process.env.BUSTIME_API_KEY // Get unique key and store it in .env
 const SERVER_PORT = 7300
-const BUS_ROUTES = 'GRE,14,31,GOL,15'
+const BUS_ROUTES = 'GRE,14,31,15'
 
 // Format and options for the request
 var request_options = {
